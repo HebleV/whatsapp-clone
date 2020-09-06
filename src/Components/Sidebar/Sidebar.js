8c2b95bef734452f1530bce9c780fe1601823a98
HebleV <visheble@yahoo.com>
2020-09-07 02:53:59 +0530

    Added the sidebarChats component

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "./Sidebar.css";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
-import ChatIcon from '@material-ui/icons/Chat';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import Avatar from '@material-ui/core/Avatar';
-import { IconButton } from '@material-ui/core';
-import { SearchOutlined } from '@material-ui/icons';
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import Avatar from "@material-ui/core/Avatar";
+import { IconButton } from "@material-ui/core";
+import { SearchOutlined } from "@material-ui/icons";
+import SidebarChat from "../SidebarChat/SidebarChat";
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-          <Avatar src="https://api.adorable.io/avatars/285/abott@adorable.png"/>
+        <Avatar src="https://api.adorable.io/avatars/285/abott@adorable.png" />
         <div className="sidebar__headerRight">
           <IconButton>
             <DonutLargeIcon />
@@ -25,10 +26,16 @@ const Sidebar = () => {
       </div>
 
       <div className="sidebar__search">
-          <div className="sidebar__searchContainer">
-              <SearchOutlined />
-              <input type='text' placeholder='Search or start new text' />
-          </div>
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input type="text" placeholder="Search or start new text" />
+        </div>
+      </div>
+
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
       </div>
     </div>
   );
