ffe4ff66cdfe66673ba7a90125e491e0886e157b
HebleV <visheble@yahoo.com>
2020-09-06 22:13:56 +0530

    Added basic components Chat and Sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Chat from "./Components/Chat/Chat";
 
 function App() {
   return (
-    <div className="App">
-      <h1>WhatsApp</h1>
+    <div className="app">
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 }
