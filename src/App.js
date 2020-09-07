324f5733dfaa4e6c42830f20adcfd3551f99e47a
HebleV <visheble@yahoo.com>
2020-09-08 03:44:04 +0530

    Added pusher to App.js and also sync API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Chat from "./Components/Chat/Chat";
+import Pusher from 'pusher-js';
+import axios from './axios';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(()=> {
+    axios.get('/messages/sync').then((response) => {
+      setMessages(response.data);
+    })
+  }, []);
+  useEffect(()=>{
+    const pusher = new Pusher('d374b5213eb327c9ae41', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(newMessage) {
+      setMessages([...messages, newMessage]);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  },[messages])
+  console.log(messages);
   return (
     <div className="app">
       <div className="app__body">
         <Sidebar />
-        <Chat />
+        <Chat messages={messages} />
       </div>
     </div>
   );
